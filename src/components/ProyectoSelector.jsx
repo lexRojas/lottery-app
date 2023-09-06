a18988208c29772c50dc3e24f66b37d667fb6060
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-09-06 10:28:28 -0600

    cambios varios

--- src/components/ProyectoSelector.jsx
+++ src/components/ProyectoSelector.jsx
@@ -1,53 +1,71 @@
 import React, { useContext, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { useForm } from "react-hook-form";
 import Context from "../context/Context";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import ModalDialog from "./ModalDialog";
+import { useModal } from "../hooks/useModal";
+
 
 function ProyectoSelector() {
-  const { get_presupuesto, tb_presupuesto } = useContext(Context);
-  const { register,handleSubmit } = useForm();
+  const { get_presupuesto, tb_presupuesto, set_Id_proyecto } = useContext(Context);
+  const { register, handleSubmit, watch } = useForm();
+  const navegate = useNavigate()
+  const [isOpen,openModal,closeModal] = useModal(false)
 
+  const onSubmit = handleSubmit((data) => {
+      const {id_proyecto} = data    
+      if (id_proyecto==="No"){
+        console.log("llamado a openModal")
+        openModal();
+      }else{
+        const {proyecto} =  tb_presupuesto.find((filas) => filas.presupuesto === id_proyecto);
+        set_Id_proyecto(id_proyecto,proyecto) 
+        navegate("/app")
+      }
+  });
 
-  const onSubmit=  handleSubmit(data =>{
-      const {filtro} = data
-      get_presupuesto(filtro);
-      console.log('filtrando')
-      console.log(filtro)
-  })
+  const filtrar_datos = () => {
+    let filtro = watch("filtro");
+    get_presupuesto(filtro);
+  };
 
   useEffect(() => {
     get_presupuesto();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
   return (
-    <Container>
-      <Row className="contenedor">
-        <Col lg={5} xs={10} className="bordes fondo-windows">
-          <Form   onSubmit={onSubmit}>
-            <Form.Group className="m-3" controlId="frmFiltro">
-              <Row>
-                <Col lg={9}>
-                  <Form.Label>Filtro de Busqueda de proyecto </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Digite un nombre..."
-                    {...register("filtro")}
-                  />
-                </Col >
-                <Col lg={3} className="d-flex flex-row-reverse align-items-end" >
-                  <Button type="submit" className="mt-2"> Filtrar </Button>
-                </Col>
-              </Row>
-            </Form.Group>
-            <Form.Group>
+    <div className="fondo-windows bordes col-md-8 container-fluid">
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="m-3" controlId="frmFiltro">
+          <div className="row">
+            <div className="col-md-10">
+              <Form.Label>Filtro de Busqueda de proyecto </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Digite un nombre..."
+                {...register("filtro")}
+              />
+            </div>
+            <div className="col-md-2  all-center">
+              <Button type="buttom" onClick={filtrar_datos}>
+                {" "}
+                Filtrar{" "}
+              </Button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
               <Form.Label>Seleccione el proyecto</Form.Label>
-              <Form.Select>
+              
+              <Form.Select id="select_proyecto" 
+                {...register("id_proyecto")}
+              >
+                <option key="-1" value="No">
+                  Seleccione un proyecto
+                </option>
                 {tb_presupuesto.length > 0 ? (
                   tb_presupuesto.map(({ presupuesto, proyecto }, i) => (
                     <option key={i} value={presupuesto}>
@@ -55,14 +73,24 @@ function ProyectoSelector() {
                     </option>
                   ))
                 ) : (
-                  <option value="No"> Cargando proyectos...</option>
+                  <option key="-99" value="No">
+                    No hay proyectos con ese nombre...
+                  </option>
                 )}
               </Form.Select>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 all-center pt-3 ">
+              <button className=" btn btn-primary " type="submit">
+                SELECCIONAR PROYECTO
+              </button>
+            </div>
+            <ModalDialog isOpen={isOpen} closeModal={closeModal}  titulo="Sistema Horas Miller" mensaje="Debes seleccionar algo" />
+          </div>
+        </Form.Group>
+      </Form>
+    </div>
   );
 }
 
